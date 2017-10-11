1905b7a69208cbb5c241a511a6b9f9529d947542
egevirgen <egevirgen@gmail.com>
2017-10-11 21:27:02 +0300

    login sayfasının javascripti

---
+++ scripts/login.js
@@ -0,0 +1,19 @@
+// oturum açma click listener
+var oturum_ac = document.getElementById("oturum_ac");
+oturum_ac.addEventListener('click', function () {
+   
+}, false);
+
+// kayıt olma click listener
+var hesap_olustur = document.getElementById("hesap_olustur");
+hesap_olustur.addEventListener('click', function () { 
+    
+    window.open ('sign_up.html','_self',false)
+
+}, false);
+
+// şifre unuttum click listener
+var sifre_unuttum = document.getElementById("forgot_password");
+sifre_unuttum.addEventListener('click', function () { 
+    
+}, false);
