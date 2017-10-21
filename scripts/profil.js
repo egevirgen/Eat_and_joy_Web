8c836c99535e93c4dde96c8e6c93de0b24b1eea0
egevirgen <egevirgen@gmail.com>
2017-10-22 01:11:59 +0300

    Add files via upload

--- scripts/profil.js
+++ scripts/profil.js
@@ -1,6 +1,8 @@
 var profile_dialog = document.getElementById("profile_dialog");
 var dialog = document.querySelector('dialog');
 var signout = document.getElementById("ileri");
+var progress = document.getElementById("progress");
+var content = document.getElementById("content");
 
 var dialogprofilephoto = document.getElementById("dialog-profile-photo");
 var dialogprofilename = document.getElementById("dialog-profile-name");
@@ -10,12 +12,25 @@ var firmaadivalue = document.getElementById("firma_adi_value");
 var firmaemailvalue = document.getElementById("firma_email_value");
 var firmatelefonvalue = document.getElementById("firma_telefon_value");
 
+var inner_firma_adi = document.getElementById("firma_adi_profile");
+var kaydet_1 = document.getElementById("kaydet_1");
+var iptal_1 = document.getElementById("iptal_1");
+var summary_1 = document.getElementById("summary_1");
+var hata_1 = document.getElementById("hata1");
+var firebase_user;
+
+
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-      return firebase.database().ref('/Companies/' + user.uid).once('value').then(function(snapshot) {
+      firebase_user=user;
+      return firebase.database().ref('/Companies/' + user.uid).once('value').then(function(snapshot){
+    progress.style.visibility='hidden';  
+    content.style.opacity='1'
     dialogprofilename.innerHTML=snapshot.val().company_name
     dialogprofileemail.innerHTML=snapshot.val().company_email
     firmaadivalue.innerHTML=snapshot.val().company_name
+    inner_firma_adi.value=snapshot.val().company_name
     firmaemailvalue.innerHTML=snapshot.val().company_email
     firmatelefonvalue.innerHTML=snapshot.val().company_phone
 });
@@ -28,6 +43,30 @@ signout.addEventListener('click', function (){
                           firebase.auth().signOut().then(function() {
             window.open ('index.html','_self',false)
         }); });
+
+iptal_1.addEventListener('click', function (){
+            inner_firma_adi.value=firmaadivalue.innerHTML;
+           progress.style.visibility='hidden';  
+           summary_1.click();
+});
+
+kaydet_1.addEventListener('click', function (){
+           if(inner_firma_adi.value === ""){
+               hata1.innerHTML = "Firma adı boş olamaz"
+           }
+    else{
+         progress.style.visibility='visible';  
+         firebase.database().ref('Companies/' + firebase_user.uid).update({
+    company_name: inner_firma_adi.value     
+  }).then(function() {
+            progress.style.visibility='hidden';
+            summary_1.click();
+            firmaadivalue.innerHTML=inner_firma_adi.value
+            dialogprofilename.innerHTML=inner_firma_adi.value
+        });
+   
+
+}});
 
 
 if (! dialog.showModal) {
